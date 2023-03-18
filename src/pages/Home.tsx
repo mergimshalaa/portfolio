d8a8f762d3ba2ca955f6f67a8b675ba9ec3212bb
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-18 14:05:11 +0100

    fix styling for headers and remove pic on homepg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -56,54 +56,43 @@ export function Home() {
           <Subtitle>{currentWord}</Subtitle>
         </Content>
       </Container>
-      <ImageWrap>
-        <img
-          style={{ boxShadow: "0px 10px 30px rgba(0, 0, 0, 0.5)" }}
-          src={myImage}
-          alt="My Image"
-        />
-      </ImageWrap>
-      <Footer />
     </>
   );
 }
 
+
+
 const Container = styled.div`
   display: flex;
-  flex: start;
-  align-items: center;
-  position: fixed;
-  bottom: 0;
-  margin-bottom: 15rem;
+  align-items: flex-end;
+  height: 60vh;
 
-  @media (max-width: 470px) {
-    flex-direction: column;
-    justify-content: center;
-  }
 `;
 
 const Content = styled.div`
-  margin-left: 50px;
+  margin-right: 50px;
 
-  @media (max-width: 470px) {
-    margin-left: 10px;
-    margin-bottom: 12rem;
+  @media (max-width: 767px) {
+    margin-right: 0;
   }
-  @media (max-width: 1193px) {
-    margin-left: 10px;
-    margin-bottom: 12rem;
-  }
+
 `;
 
 const Title = styled.h1`
   font-size: 4rem;
   letter-spacing: 3px;
   color: white;
-  margin-bottom: 0;
+  margin-bottom: 1rem;
   vertical-align: top;
+
+  @media (max-width: 875px) {
+    font-size: 3rem;
+  }
 
   @media (max-width: 575px) {
     font-size: 3rem;
+    margin-bottom: 0.5rem;
+    margin-left: 10px;
   }
 `;
 
@@ -111,41 +100,27 @@ const Subtitle = styled.h2`
   letter-spacing: 1.5px;
   font-size: 3rem;
   color: white;
-  margin-top: 0;
+  margin: 0;
   height: 4.5rem;
   vertical-align: top;
 
-  @media (max-width: 470px) {
+  @media (max-width: 875px) {
+    font-size: 2.5rem;
+    height: 3.5rem;
+  }
+
+  @media (max-width: 575px) {
     font-size: 2rem;
+    height: auto;
+    margin-left: 10px;
+    height: 3.5rem;
+  }
+
+  @media (max-width: 400px) {
+    font-size: 1.9rem;
   }
 `;
 
 const FirstLetterStyle = styled.span`
   color: #a4907c;
-`;
-
-const ImageWrap = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: end;
-  margin-top: 12rem;
-  opacity: 0.7;
-  max-width: 100%;
-  // box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);
-
-  @media (max-width: 768px) {
-    order: -1;
-  }
-
-  img {
-    height: auto;
-    opacity: 0.7; /* Add opacity to the image */
-  }
-
-  @media (max-width: 575px) {
-    widht: 145px;
-    height: 260px;
-    padding: 5px 0px;
-    margin-top: 19.2rem;
-  }
 `;
