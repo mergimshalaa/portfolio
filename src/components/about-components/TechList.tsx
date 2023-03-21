b0de3e0c7242fee59dd2d6d42e2562fe3330f1f8
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-21 09:39:36 +0100

    finish techlist with icons

--- src/components/about-components/TechList.tsx
+++ src/components/about-components/TechList.tsx
@@ -3,20 +3,16 @@ import { FaCss3Alt, FaHtml5 } from "react-icons/fa";
 import { SiTypescript, SiReact, SiSass } from "react-icons/si";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faVuejs, faNodeJs } from "@fortawesome/free-brands-svg-icons";
+import { BsFillBootstrapFill } from "react-icons/bs";
 import "./techlist.css";
 
 export function TechList() {
   return (
     <Container className="text-center text-icons">
-      <h2
-
-      >
-        SKILLS
-      </h2>
+      <h2>SKILLS</h2>
       <Row
         style={{
           justifyContent: "center",
-          paddingBottom: "50px",
           fontSize: "3rem",
           color: "white",
         }}
@@ -25,22 +21,25 @@ export function TechList() {
           <SiReact style={{ color: "#61DBFB" }} />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
+          <FontAwesomeIcon icon={faVuejs} style={{ color: "#4FC08D" }} />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
           <SiTypescript style={{ color: "#007ACC" }} />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
-          <FaHtml5 style={{ color: "#E34F26" }} />
+          <FontAwesomeIcon icon={faNodeJs} style={{ color: "#68A063" }} />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
-          <FaCss3Alt style={{ color: "#1572B6" }} />
+          <BsFillBootstrapFill style={{ color: "#0d6efd" }} />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
           <SiSass style={{ color: "#CC6699" }} />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
-          <FontAwesomeIcon icon={faVuejs} style={{ color: "#4FC08D" }} />
+          <FaCss3Alt style={{ color: "#1572B6" }} />
         </Col>
         <Col xs={4} md={2} className="tech-icons">
-          <FontAwesomeIcon icon={faNodeJs} style={{ color: "#68A063" }} />
+          <FaHtml5 style={{ color: "#E34F26" }} />
         </Col>
       </Row>
     </Container>
