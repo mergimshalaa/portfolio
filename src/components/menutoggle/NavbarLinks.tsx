f69635dc4b2d75200fd0cc4c57b5374e2e309059
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-27 18:41:09 +0200

    fix issue

--- src/components/menutoggle/NavbarLinks.tsx
+++ src/components/menutoggle/NavbarLinks.tsx
@@ -4,30 +4,30 @@ import styled from "styled-components";
 import { FaHome, FaUser, FaProjectDiagram, FaFileAlt } from "react-icons/fa";
 
 
-interface NavbarLinksProps {
+
+  interface NavbarLinksProps {
     handleClose: () => void;
   }
   
-
-export function NavbarLinks({ handleClose }: NavbarLinksProps)  {
-  return (
-    <Nav className="flex-column">
-      <StyledLink onClick={handleClose} to="/" exact={true.toString()} as={NavLink}>
-        <FaHome style={{ marginRight: "5px" }} /> Home
-      </StyledLink>
-      <StyledLink onClick={handleClose} to="/about" as={NavLink} >
-        <FaUser style={{ marginRight: "5px" }} /> About
-      </StyledLink>
-      <StyledLink onClick={handleClose} to="/projects" as={NavLink} >
-        <FaProjectDiagram style={{ marginRight: "5px" }} /> Projects
-      </StyledLink>
-      <StyledLink onClick={handleClose} to="/resume" as={NavLink}>
-        <FaFileAlt style={{ marginRight: "5px" }} /> Resume
-      </StyledLink>
-    </Nav>
-  );
-}
-
+  export function NavbarLinks({ handleClose }: NavbarLinksProps)  {
+    return (
+      <Nav className="flex-column">
+        <StyledLink onClick={handleClose} to="/" exact as={NavLink}>
+          <FaHome style={{ marginRight: "5px" }} /> Home
+        </StyledLink>
+        <StyledLink onClick={handleClose} to="/about" as={NavLink} >
+          <FaUser style={{ marginRight: "5px" }} /> About
+        </StyledLink>
+        <StyledLink onClick={handleClose} to="/projects" as={NavLink} >
+          <FaProjectDiagram style={{ marginRight: "5px" }} /> Projects
+        </StyledLink>
+        <StyledLink onClick={handleClose} to="/resume" as={NavLink}>
+          <FaFileAlt style={{ marginRight: "5px" }} /> Resume
+        </StyledLink>
+      </Nav>
+    );
+  }
+  
 
 const StyledLink = styled(NavLink)<{ exact?: boolean }>`
   color: white;
