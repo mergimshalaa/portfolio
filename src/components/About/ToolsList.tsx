f6d498d74e3188f4261436c78edb542bf2e648e3
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-28 00:02:56 +0200

    added git & github icons

--- src/components/About/ToolsList.tsx
+++ src/components/About/ToolsList.tsx
@@ -1,11 +1,11 @@
 import { Col, Container, Row } from "react-bootstrap";
 import "./techlist.css";
-import { DiVisualstudio, FiFigma } from "react-icons/all";
+import { DiVisualstudio, FiFigma, DiGit, FaGithub } from "react-icons/all";
 
 export function ToolsList() {
   return (
     <Container className="text-center text-icons">
-      <h2>
+      <h2 className="header-tools">
         TOOLS
       </h2>
       <Row
@@ -14,10 +14,17 @@ export function ToolsList() {
           paddingBottom: "50px",
           fontSize: "3rem",
           color: "white",
+          gap: '.3rem'
         }}
       >
       <Col xs={4} md={2} className="tech-icons">
           <DiVisualstudio style={{ color: "#007ACC" }} />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <DiGit style={{ color: "red" }} />
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+          <FaGithub/>
         </Col>
         <Col xs={4} md={2} className="tech-icons">
           <FiFigma style={{ color: "#F24E1E" }} />
