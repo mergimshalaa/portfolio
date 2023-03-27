69179d563ed4383663f666b1c0582597340d8b93
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-03-27 21:55:21 +0200

    less margin on the text on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -96,7 +96,7 @@ const Title = styled.h1`
   @media (max-width: 575px) {
     font-size: 3rem;
     margin-bottom: 0.5rem;
-    margin-left: 10px;
+    margin-left: 7px;
   }
 `;
 
@@ -116,7 +116,7 @@ const Subtitle = styled.h2`
   @media (max-width: 575px) {
     font-size: 2rem;
     height: auto;
-    margin-left: 10px;
+    margin-left: 7px;
     height: 3.5rem;
   }
 
