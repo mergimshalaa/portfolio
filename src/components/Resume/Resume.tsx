735d395002b7a6266a89048ca3d0d7498ff10f98
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-04-14 00:26:03 +0200

    animation for headers

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -38,17 +38,19 @@ const Styledh1 = styled.h1`
   background-color: #fff;
   padding: 12px 24px;
   border-radius: 4px;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.15);
   display: inline-block;
   letter-spacing: 2px;
-  transition: all 0.2s ease-in-out;
+  opacity: 0;
+  animation: fadeIn 1.5s ease-in-out forwards;
 
-  &:hover {
-    background-color: #f1f1f1;
-    color: #333;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
-
   @media (max-width: 768px) {
     font-size: 28px;
     padding: 10px 16px;
