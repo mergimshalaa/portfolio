b571a0c3fc9459215de24d235b2a312578bbd5e1
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-04-19 00:25:21 +0200

    state for loading page with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom";
-import { Home } from '../src/pages/Home/Home';
-import { About } from '../src/pages/About/About';
-import { Projects } from '../src/pages/Projects/Projects';
-import { Resume } from '../src/pages/Resume/Resume';
-import { Navbar } from '../src/components/Navbar/Navbar';
-import { Container } from 'react-bootstrap';
-import { Footer } from './components/Footer';
+import { Home } from "../src/pages/Home/Home";
+import { About } from "../src/pages/About/About";
+import { Projects } from "../src/pages/Projects/Projects";
+import { Resume } from "../src/pages/Resume/Resume";
+import { Navbar } from "../src/components/Navbar/Navbar";
+import { Container } from "react-bootstrap";
+import { Footer } from "./components/Footer";
+
+import { Loader } from "./components/Loader";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3200);
+  }, []);
+
   return (
     <>
-      <Navbar />
-      <Container>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
-      </Container>
-      <Footer />
+      {isLoading ? (
+        <Loader onFinish={() => setIsLoading(false)} />
+      ) : (
+        <>
+          <Navbar />
+          <Container>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/resume" element={<Resume />} />
+            </Routes>
+          </Container>
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
