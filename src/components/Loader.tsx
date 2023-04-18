d0cb1ec846c1027e74c01eeb21fb0b0792a41983
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2023-04-19 00:53:56 +0200

    loaderpage with animation for logo

---
+++ src/components/Loader.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+import logopng from "../img/logo.png";
+import anime from "animejs/lib/anime.es.js";
+
+interface Props {
+  onFinish: () => void;
+}
+
+export const Loader: React.FC<Props> = ({ onFinish }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const logoRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), 10);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      animateLogo();
+    }
+  }, [isMounted]);
+
+  const animateLogo = () => {
+    anime({
+      targets: logoRef.current,
+      opacity: [0, 1],
+      duration: 1100,
+      easing: "linear",
+      complete: () => {
+        anime({
+          targets: logoRef.current,
+          opacity: [1, 0],
+          duration: 1000,
+          easing: "linear",
+          complete: onFinish,
+        });
+      },
+    });
+  };
+
+  return (
+    <>
+      <StyledLoader>
+        <StyledLogo ref={logoRef} src={logopng} alt="Logo" />
+      </StyledLoader>
+    </>
+  );
+};
+
+const StyledLogo = styled.img`
+  width: 25rem;
+  height: 25rem;
+`;
+
+const StyledLoader = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
